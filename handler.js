204c92a3f3b17d09c90573d7b3c383c3cbc79826
Patrick Brandt <patrickbrandt@Patricks-MBP.home>
2016-12-27 15:05:12 -0500

    adding in redirect

--- handler.js
+++ handler.js
@@ -1,13 +1,19 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Ping!',
-      input: event,
-    })
-  };
-
-  callback(null, response);
+module.exports.redirect = (event, context, callback) => {
+  let params = event.pathParameters;
+  if (!!params && !!params.id) {
+    let id = params.id;
+    callback(null, {
+      statusCode: 301,
+      headers: { //TODO: pull location url from DynamoDB using id
+        'Location': 'http://banditbrandit.com/'
+      }
+    });
+  } else {
+    callback(null, {
+      statusCode: 200,
+      body: 'nothing to see here!'
+    });
+  }
 };
