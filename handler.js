65bf263c8baa9bfc6e002a2a767ad168b6c8fc0b
Patrick Brandt <patrickbrandt@Patricks-MBP.hsd1.ga.comcast.net>
2018-04-18 22:00:34 -0400

    returning wpb.is/PatrickBrandt markup

--- handler.js
+++ handler.js
@@ -2,6 +2,7 @@
 
 const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient();
+const s3 = new AWS.S3();
 const URLS_TABLE = 'Urls';
 
 module.exports.redirect = (event, context, callback) => {
@@ -17,8 +18,33 @@ module.exports.redirect = (event, context, callback) => {
     body: 'nothing to see here!'
   };
 
-  if (!!pathParams && !!pathParams.id) {
-    getUrl(pathParams.id)
+  if (!pathParams || !pathParams.id) {
+    return callback(null, noRedirect);
+  }
+
+  const id = pathParams.id.toLowerCase();
+  if (id === 'patrickbrandt') {
+    const params = {
+      Bucket: process.env.wpbis_bucket,
+      Key: "index.html"
+     };
+     s3.getObject(params).promise()
+      .then(data => {
+        const response = {
+          statusCode: 200,
+          headers: {
+            'Content-Type': 'text/html',
+          },
+          body: data.Body.toString(),
+        };
+        callback(null, response);
+      })
+      .catch(err => {
+        console.log(err);
+        callback(null, getError());
+      });
+  } else {
+    getUrl(id)
       .then(url => {
         if(!!url) {
           return callback(null, {
@@ -27,22 +53,16 @@ module.exports.redirect = (event, context, callback) => {
               'Cache-Control': noCache['Cache-Control'],
               Pragma: noCache.Pragma,
               Expires: noCache.Expires,
-              Location: url
+              Location: url,
             }
           });
         }
-
         callback(null, noRedirect);
       })
       .catch(err => {
         console.log(err);
-        callback(null, {
-          statusCode: 500,
-          body: 'something bad happened'
-        });
+        callback(null, getError());
       });
-  } else {
-    callback(null, noRedirect);
   }
 };
 
@@ -50,13 +70,19 @@ function getUrl(id) {
   return new Promise((resolve, reject) => {
     const params = {
       TableName: URLS_TABLE,
-      Key: { Id: id.toLowerCase() }
+      Key: { Id: id },
     };
-
     docClient.get(params, (err, data) => {
       if (err) return reject(err);
       let url = data.Item ? data.Item.Url : undefined;
       resolve(url);
     });
   });
+}
+
+function getError() {
+  return  {
+    statusCode: 500,
+    body: 'something bad happened',
+  }
 }
